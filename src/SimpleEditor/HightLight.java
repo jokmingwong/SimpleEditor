134c210919952ae7a8f03e07472ce472867d0512
Adam <jokmingwong@gmail.com>
2019-05-31 11:09:49 +0800

    Update and finish the HighLight.java
    Signed-off-by: adamwong <jokmingwong@gmail.com>

--- src/SimpleEditor/HightLight.java
+++ src/SimpleEditor/HightLight.java
@@ -11,6 +11,22 @@ public class HightLight extends DefaultHighlighter.DefaultHighlightPainter {
     }
 
     public void paint(JTextComponent textComponent, String[] pattern) {
+        try {
+            cancelPaint(textComponent);
+            Highlighter highlighter = textComponent.getHighlighter();
+            Document document = textComponent.getDocument();
+            String text = document.getText(0, document.getLength());
+            for (String p : pattern) {
+                int pos = 0;
+                while ((pos = text.indexOf(p, pos)) >= 0) {
+                    highlighter.addHighlight(pos, pos + p.length(), this);
+                    pos += p.length();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
 
