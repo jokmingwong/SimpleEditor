f79e5792b3fa5226024b6abdf6b4284e45d2fbae
khunkin <657828489@qq.com>
2019-06-04 16:00:57 +0800

    update autocomplete

--- src/SimpleEditor/SupportedKeywords.java
+++ src/SimpleEditor/SupportedKeywords.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+
 /**
  * <h1>A class to store the programming language keywords and
  * provide access to them.</h1>
@@ -14,33 +15,9 @@ import java.util.Map;
  */
 public class SupportedKeywords {
 
-    private static String[] supportedLanguages = {".cpp",".java"};
+    private static String[] supportedLanguages = {".cpp", ".java"};
 
-
-    private static String[] javaVariableType = {
-            "boolean", "byte", "char", "float", "int", "long", "short", "String", "double", "void"
-    };
-    private static String[] javaControl = {
-            "break", "case", "catch", "assert", "continue", "do", "import", "extends", "package",
-            "implements", "new", "switch", "synchronize", "if", "else", "for", "finally", "return", "super",
-            "throw", "throws", "try", "while"
-    };
-    private static String[] javaConstraint = {
-            "abstract", "class", "const", "default", "final", "private", "protected", "public", "interface", "static",
-            "this", "volatile", "friend"
-    };
-
-    private enum Type {
-        JAVA_CONSTRAINT,
-        JAVA_CONTROL,
-        JAVA_VARIABLE_TYPE,
-        CPP_CONSTRAINT,
-        CPP_CONTROL,
-        CPP_VARIABLE_TYPE
-    }
-
-    /* Delete the function javaKeyWords
-    private static String[] javaKeyWords = {"abstract", "assert", "boolean",
+    private static String[] java = {"abstract", "assert", "boolean",
             "break", "byte", "case", "catch", "char", "class", "const",
             "continue", "default", "do", "double", "else", "extends", "false",
             "final", "finally", "float", "for", "goto", "if", "implements",
@@ -49,63 +26,57 @@ public class SupportedKeywords {
             "long", "native", "return", "short", "static", "strictfp", "super", "switch",
             "synchronized", "this", "throw", "throws", "transient", "true",
             "try", "void", "volatile", "while", "String"};
-    */
 
-    private static String[] cppControl = {};
-    private static String[] cppConstraint = {};
-    private static String[] cppVariableType = {};
-
-    private static String[] cppKeyWords = {"auto", "const", "double", "float", "int", "short",
-                "struct", "unsigned", "break", "continue", "else", "for", "long", "signed",
-                "switch", "void", "case", "default", "enum", "goto", "register", "sizeof",
-            "typedef", "volatile", "char", "do", "extern", "if", "return", "using", "const_cast",
-            "inline", "public", "throw", "virtual", "delete", "mutable", "protected", "true", "wchar_t", "static",
-                "union", "while", "asm", "dynamic_cast", "namespace", "reinterpret_cast", "try",
-                "bool", "explicit", "new", "static_cast", "typeid", "catch", "false", "operator",
-            "template", "typename", "class", "friend", "private", "this"};
+    private static String[] cpp = {"auto", "const", "double", "float", "int", "short",
+            "struct", "unsigned", "break", "continue", "else", "for", "long", "signed",
+            "switch", "void", "case", "default", "enum", "goto", "register", "sizeof",
+            "typedef", "volatile", "char", "do", "extern", "if", "return", "static",
+            "union", "while", "asm", "dynamic_cast", "namespace", "reinterpret_cast", "try",
+            "bool", "explicit", "new", "static_cast", "typeid", "catch", "false", "operator",
+            "template", "typename", "class", "friend", "private", "this", "using", "const_cast",
+            "inline", "public", "throw", "virtual", "delete", "mutable", "protected", "true", "wchar_t"};
 
     public String[] getSupportedLanguages() {
         return supportedLanguages;
     }
 
-    private static String[] brackets = { "{", "[","(" };
-    private static String[] bCompletions = { "}","]", ")" };
+    private static String[] brackets = {"{", "[", "("};
+    private static String[] bCompletions = {"}", "]", ")"};
+
+
     private static HashMap<String, String> bracketMap = new HashMap<>();
+
     static {
         bracketMap.put("{", "{}");
         bracketMap.put("[", "[]");
         bracketMap.put("(", "()");
     }
 
-    public String[] getKeywords(Type t) {
-        switch (t) {
-            case JAVA_CONTROL:
-                return javaControl;
-            case JAVA_CONSTRAINT:
-                return javaConstraint;
-            case JAVA_VARIABLE_TYPE:
-                return javaVariableType;
-            case CPP_CONTROL:
-                return cppControl;
-            case CPP_CONSTRAINT:
-                return cppConstraint;
-            case CPP_VARIABLE_TYPE:
-                return cppVariableType;
-        }
-        return null;
+    public String[] getJavaKeywords() {
+        return java;
+    }
+
+    public String[] getCppKeywords() {
+        return cpp;
     }
 
     public ArrayList<String> getBracketCompletions() {
         return new ArrayList<>(Arrays.asList(bCompletions));
     }
+
     public ArrayList<String> getBrackets() {
         return new ArrayList<>(Arrays.asList(brackets));
     }
+
     public ArrayList<String> setKeywords(String[] arr) {
         return new ArrayList<>(Arrays.asList(arr));
     }
 
-    public Map<String, String> getBracketMap(){
+    public static void setBracketMap(HashMap<String, String> bracketMap) {
+        SupportedKeywords.bracketMap = bracketMap;
+    }
+
+    public Map<String, String> getBracketMap() {
         return bracketMap;
     }
 
