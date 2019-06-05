fb4796ad0920fc5f19d69d4bd5a8804ff2e10908
linan1109 <40530883+linan1109@users.noreply.github.com>
2019-06-05 20:39:54 +0800

    Update ClearAll.java

--- src/SimpleEditor/ClearAll.java
+++ src/SimpleEditor/ClearAll.java
@@ -1,5 +1,9 @@
 package SimpleEditor;
 import javax.swing.JTextArea;
+/*
+ * 清屏，不说什么了
+ * @author LiNan
+ * */
 public class ClearAll {
     public static void clear(JTextArea textArea) {
         textArea.setText("");
